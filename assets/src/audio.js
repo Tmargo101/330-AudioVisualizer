5e223301a0cd3b4e8740c06e7e6fe66354d5bf6d
Thomas Margosian <tmargo101@gmail.com>
2020-10-19 16:26:54 -0400

    first commit to AudioBuffer branch.

--- assets/src/audio.js
+++ assets/src/audio.js
@@ -1,10 +1,7 @@
-// 1 - our WebAudio context, **we will export and make this public at the bottom of the file**
 let audioCtx;
-let currentBuffer = null;
+let element;
 
-// **These are "private" properties - these will NOT be visible outside of this module (i.e. file)**
-// 2 - WebAudio nodes that are part of our WebAudio audio routing graph
-let element, sourceNode, analyserNode, gainNode;
+let source = null;
 
 // 3 - here we are faking an enumeration
 const DEFAULTS = Object.freeze({
@@ -12,11 +9,6 @@ const DEFAULTS = Object.freeze({
    numSamples : 256
 });
 
-// 4 - create a new array of 8-bit integers (0-255)
-// this is a typed array to hold the audio frequency data
-let audioData = new Uint8Array(DEFAULTS.numSamples / 2);
-
-// **Next are "public" methods - we are going to export all of these at the bottom of this file**
 const setupWebAudio = (filePath) => {
 
    // 1 - The || is because WebAudio has not been standardized across browsers yet
@@ -29,42 +21,37 @@ const setupWebAudio = (filePath) => {
    // 3 - have it point at a sound file
    loadSoundFile(filePath);
 
-   // 4 - create an a source node that points at the <audio> element
-   sourceNode = audioCtx.createMediaElementSource(element);
+   source = audioCtx.createBufferSource();
 
-   // 5 - create an analyser node
-   analyserNode = audioCtx.createAnalyser();    // note the UK spelling of "Analyser"
+   // Create the XHR which will grab the audio contents
+   var request = new XMLHttpRequest();
+   // Set the audio file src here
+   request.open('GET', filePath, true);
+   // Setting the responseType to arraybuffer sets up the audio decoding
+   request.responseType = 'arraybuffer';
+   request.onload = function() {
+     // Decode the audio once the require is complete
+     audioCtx.decodeAudioData(request.response, function(newBuffer) {
+       source.buffer = newBuffer;
+       // Connect the audio to source (multiple audio buffers can be connected!)
+       //currentBuffer.connect(audioCtx.destination);
+       // Simple setting for the buffer
+       //currentBuffer.loop = true;
+       // Play the sound!
+       //currentBuffer.start(0);
+     }, function(e) {
+       console.log('Audio error! ', e);
+     });
+   }
+
+   // Send the request which kicks off
+   request.send();
+
+console.table(currentBuffer);
 
 
-   /*
-   // 6
-   We will request DEFAULTS.numSamples number of samples or "bins" spaced equally
-   across the sound spectrum.
-
-   If DEFAULTS.numSamples (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz,
-   the third is 344Hz, and so on. Each bin contains a number between 0-255 representing
-   the amplitude of that frequency.
-   */
-
-   // fft stands for Fast Fourier Transform
-   analyserNode.fftSize = DEFAULTS.numSamples;
-
-   // 7 - create a gain (volume) node
-   gainNode = audioCtx.createGain();
-   gainNode.gain.value = DEFAULTS.gain;
-
-   // 8 - connect the nodes - we now have an audio graph
-   sourceNode.connect(analyserNode);
-   analyserNode.connect(gainNode);
-   gainNode.connect(audioCtx.destination);
 
 };
-
-const showBuffer = (element) => {
-   element.arrayBuffer();
-   audioCtx.decodeAudioData(arrayBuffer);
-   console.table(arrayBuffer);
-}
 
 const loadSoundFile = (filePath) => {
    element.src = filePath;
--- assets/src/visualizer.js
+++ assets/src/visualizer.js
@@ -2,57 +2,16 @@ import * as utils from './utils.js';
 
 let ctx,canvasWidth,canvasHeight,gradient,analyserNode,audioData;
 
-const setupCanvas = (canvasElement,analyserNodeRef) => {
+const setupCanvas = (canvasElement) => {
 	// create drawing context
 	ctx = canvasElement.getContext("2d");
 	canvasWidth = canvasElement.width;
 	canvasHeight = canvasElement.height;
 	// create a gradient that runs top to bottom
-
-	// keep a reference to the analyser node
-   analyserNode = analyserNodeRef;
-
-	// this is the array where the analyser data will be stored
-   audioData = new Uint8Array(analyserNode.fftSize/2);
 };
 
 const draw = (drawParams = {}) => {
-   // Drawing code goes here
-   analyserNode.getByteFrequencyData(audioData);
-
-   ctx.save();
-   ctx.fillStyle = "black";
-   ctx.globalAlpha = 0.1;
-   ctx.fillRect(0,0,canvasWidth, canvasHeight);
-   ctx.restore();
-
-   let barSpacing = 4;
-   let margin = 5;
-   let screenWidthForBars = canvasWidth - (audioData.length * barSpacing) - margin * 2;
-   let barWidth = screenWidthForBars / audioData.length;
-   let barHeight = 500;
-   let topSpacing = 100;
-
-   ctx.save();
-   ctx.fillStyle = 'rgba(255,255,255,0.5)';
-   ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-   for (let i = 0; i < audioData.length; i++) {
-      ctx.fillRect(
-         margin + i * (barWidth + barSpacing),
-         topSpacing + 256 - audioData[i],
-         barWidth,
-         barHeight
-      );
-      ctx.strokeRect(
-         margin + i * (barWidth + barSpacing),
-         topSpacing + 256 - audioData[i],
-         barWidth,
-         barHeight
-      );
-
-   }
-   ctx.restore();
-
+   
 };
 
 export {
